58ecd5fbb4a6430042a0216d9cac9ecf8fe5930b
Shubham Minglani <shubham@linux.com>
2017-11-05 18:43:55 +0530

    reduce timeout

--- plugin.sh
+++ plugin.sh
@@ -1,3 +1,3 @@
 exec 3>&2
-trap 'exec 2> >(tee >(read -t 0.01 stdin; DARN_ERROR=$stdin goclient-rest &> /dev/null))' DEBUG
+trap 'exec 2> >(tee >(read -t 0.3 stdin; DARN_ERROR=$stdin goclient-rest &> /dev/null))' DEBUG
 PROMPT_COMMAND='exec 2>&3'
